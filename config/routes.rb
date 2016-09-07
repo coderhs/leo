efe435d82b316d92528493c8222ac169ef6ad1df
Harisankar P S <mailme@hsps.in>
2016-09-07 13:14:01 +0530

    Change resource from website to websites

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   api_version(:module => "V1", :path => {:value => "v1"}, :defaults => {:format => "json"}) do
-    resources :website, only: [:index, :create] do
+    resources :websites, only: [:index, :create] do
       collection do
         match ':key', to: 'website#show', via: [:get], as: 'result'
       end
