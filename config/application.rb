270364af36da93d8e14ae1dc83e7462904b8d087
Harisankar P S <mailme@hsps.in>
2016-09-07 10:43:27 +0530

    Remove unrequired generators

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module Leo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+      g.view_specs false
+    end
   end
 end
